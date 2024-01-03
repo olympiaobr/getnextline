1b0c0a9aa6d35ae466704bb21012dedc14bbbfb4
olympiaobr <134846910+olympiaobr@users.noreply.github.com>
2024-01-03 12:45:17 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,27 +9,17 @@ char	*get_next_line(int fd);
 int	main(void)
 {
 	int		fd;
-	int		fd_two;
-	int		index;
 	char	*gnl;
 
 	fd = open("test1.txt", O_RDONLY);
-	fd_two = open("test2.txt", O_RDONLY);
-	index = 9;
-	while (index--)
+	while (1)
 	{
 		gnl = get_next_line(fd);
-		if (gnl)
-			printf("%s", gnl);
-		else
-			printf("%s", "NULL");
-		free(gnl);
-		gnl = get_next_line(fd_two);
-		if (gnl)
-			printf("%s", gnl);
-		else
-			printf("%s", gnl);
+		if (!gnl)
+			break ;
+		printf("%s", gnl);
 		free(gnl);
 	}
 	close(fd);
+	return (0);
 }
